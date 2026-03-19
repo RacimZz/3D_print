7c6a3fa06db5be3a3c2ea14c0f8a547381c235b5
Racim Zenati <racimzenati@gmail.com>
2026-03-19 14:22:04 +0100

    fonctions auxiliares

--- minigeo/classification.py
+++ minigeo/classification.py
@@ -13,14 +13,68 @@ class Noeud:
         """
         pass
 
-
+class Arbre:
+    def __init__(self,racine):
+        self.racine = racine
+        
 def arbre_inclusion(polygones):
     """
     prend un ensemble de polygones qui ne s'intersectent pas (hormis sur leur bord).
     renvoie un arbre (le noeud racine etant le plan) indiquant qui est inclu dans qui.
     pre-condition: pas de doublons, pas d'intersections hors bordures.
     """
+    racine = Noeud("PLAN")
+    arbre = Arbre(racine)
+    arbre_inclusion_iter(racine,polygones)
     pass
+
+def arbre_inclusion_iter(noeud,polygones):
+    """
+    construit les fils de la racine en mettant des polygones comme contenu des 
+    noeuds
+    """
+    if noeud is None:
+        return
+
+    pass
+
+def est_maximal(poly,polygones):
+    for polygone in polygones:
+        if polygone != poly and polygone.contient(poly) :
+            return False
+    return True
+
+"""
+- construire un dico avec {polygone : poly qui contiennent polygone}
+- si polygone : []  donc c'est le fils de "PLAN"
+- sinon : trouver le plus petit polygone parmis les poly (son pere)
+    pour pouvoir constuire un dicà = {polygone : son pere}
+"""
+def construire_hash(polygones):
+    hash_polygones = {polygone:[] for polygone in polygones}
+    for cle in polygones:
+        for contient_cle in polygones:
+            if contient_cle != cle and contient_cle.contient(cle):
+                hash_polygones[cle].append(contient_cle)
+    return hash_polygones
+
+def associer_pere(poly_fils, hash_polygones):
+    if hash_polygones[poly_fils] == [] :
+        hash_polygones[poly_fils] = "PLAN"
+        return 
+    hash_polygones[poly_fils] = trouver_plus_petit_poly(hash_polygones[poly_fils])
+     
+
+def trouver_plus_petit_poly(polygones):
+    for poly_petit in polygones:
+        petit = True
+        for contient_petit in polygones:
+            if poly_petit != contient_petit and not contient_petit.contient(poly_petit) :
+                petit = False
+                break
+        if petit : return poly_petit
+    return None 
+    # n'arrivera jamais vu que les polygones ne s'intersectent pas donc il existe un petit
 
 
 def main():
